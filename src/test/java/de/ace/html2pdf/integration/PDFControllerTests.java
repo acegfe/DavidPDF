edc8311252159a3934d260f4862747a8cd99777e
Deepthi Nayak <deepthi.nayak@ace.de>
2024-03-18 15:49:58 +0100

    Testcase using test containers

--- src/test/java/de/ace/html2pdf/integration/PDFControllerTests.java
+++ src/test/java/de/ace/html2pdf/integration/PDFControllerTests.java
@@ -1,42 +1,37 @@
 package de.ace.html2pdf.integration;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import de.ace.html2pdf.config.ApplicationValuesConfig;
-import de.ace.html2pdf.controller.PdfGenerationController;
-import org.junit.jupiter.api.Assertions;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
+@ActiveProfiles("test")
 public class PDFControllerTests {
 
   @Autowired
   private MockMvc mockMvc;
 
-  @Autowired
-  PdfGenerationController controller;
 
-
-  @Autowired
-  private ApplicationContext applicationContext;
-
-  static ApplicationValuesConfig config;
+  @Value("classpath:input.html")
+  private Resource inputFile;
 
  private static final DockerImageName IMAGENAME = DockerImageName.parse("seleniarm/standalone-chromium:latest")
      .asCompatibleSubstituteFor("selenium/standalone-chrome");
@@ -45,40 +40,27 @@ public class PDFControllerTests {
   private static final BrowserWebDriverContainer<?> chromeContainer = new BrowserWebDriverContainer<>(IMAGENAME)
         .withCapabilities(new ChromeOptions().addArguments("--headless", "--no-sandbox"));
 
+  @BeforeAll
+  static void setUp() {
+    chromeContainer.start();
+  }
+
   @DynamicPropertySource
   static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-    registry.add(config.getPath(), () -> "http://" + chromeContainer.getHost() + ":" + chromeContainer.getMappedPort(4444));
+    registry.add("dynamic.selenium.driver-path", () -> "http://" + chromeContainer.getHost() + ":" + chromeContainer.getMappedPort(4444));
   }
+
 
   @Test
-  public void testMockMvcBeanExists() {
-    Assertions.assertNotNull(applicationContext.getBean(MockMvc.class));
-  }
-
     public void givenHTML_whenAPIIsCalled_thenReturnPDF() throws Exception {
-      String html="<!DOCTYPE html>\n"
-          + "<html lang=\"en\">\n"
-          + "<head>\n"
-          + "    <meta charset=\"UTF-8\">\n"
-          + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
-          + "    <title>Test HTML Page</title>\n"
-          + "</head>\n"
-          + "<body>\n"
-          + "    <header>\n"
-          + "        <h1>Test HTML Page</h1>\n"
-          + "    </header>\n"
-          + "    <main>\n"
-          + "        <p>This is a sample HTML page for testing.</p>\n"
-          + "    </main>\n"
-          + "    <footer>\n"
-          + "        <p>Footer content goes here.</p>\n"
-          + "    </footer>\n"
-          + "</body>\n"
-          + "</html>\n";
 
-      mockMvc.perform(MockMvcRequestBuilders.post("/pdf/html")
-              .content(html))
-          .andExpect(MockMvcResultMatchers.status().isOk());
+      String input = inputFile.getContentAsString(StandardCharsets.UTF_8);
+
+      mockMvc.perform(post("/pdf/html")
+              .content(input)
+              .header("Authorization","Bearer prodkey"))
+          .andExpect(status().isOk())
+          .andExpect(header().string("Content-Type","application/pdf"));
 
     }
 
