c6e8b9d2b0b05a313b495f854c7ebfbe202a079d
Deepthi Nayak <deepthi.nayak@ace.de>
2024-03-18 12:00:57 +0100

    Test for the String input

--- src/test/java/de/ace/html2pdf/unit/PdfGenerationControllerTests.java
+++ src/test/java/de/ace/html2pdf/unit/PdfGenerationControllerTests.java
@@ -9,13 +9,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import de.ace.html2pdf.application.PdfRenderComponent;
 import de.ace.html2pdf.application.PdfService;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -31,42 +32,23 @@ public class PdfGenerationControllerTests {
   @MockBean
   private PdfRenderComponent pdfRenderComponent;
 
+  @Value("classpath:input.html")
+  private Resource inputFile;
+
   @Test
   void shouldReturnApplicationHealth() throws Exception {
     this.mockMvc.perform(get("/pdf/healthCheck")).andDo(print()).andExpect(status().isOk());
   }
 
-@Test
+  @Test
   void givenHTMLInput_whenAPICalled_thenReturnPDF() throws Exception {
-    String input = """
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <title>Sample HTML with Footer</title>
-                            <style>
-                                footer {
-                                    background-color: #f4f4f4;
-                                    padding: 20px;
-                                    text-align: center;
-                                }
-                            </style>
-                        </head>
-                        <body>
-                            <h1>Welcome to my website</h1>
-                            <p>This is a sample HTML page with a footer.</p>
-                            <footer>
-                                <p>This is the footer section.</p>
-                                <p>Contact us: info@example.com</p>
-                            </footer>
-                        </body>
-                        </html>
-                    """;
+    String input = inputFile.getContentAsString(StandardCharsets.UTF_8);
 
     byte[] byteArray = input.getBytes(StandardCharsets.UTF_8);
     when(pdfService.html2pdf(input)).thenReturn(byteArray);
-    this.mockMvc.perform(post("/pdf/html")).andExpect(status().isOk())
-        .andExpect(header().string("Content-Type","application/pdf"));
+    this.mockMvc.perform(post("/pdf/html")
+                .content(input))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type","application/pdf"));
   }
 }
