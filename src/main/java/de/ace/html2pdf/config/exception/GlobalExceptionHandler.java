33becd6e598166c3f02fd3e7502931df5d73e157
mark.shmarov <shmarov.mark@gmail.com>
2024-03-20 10:10:16 +0100

    renaming commit

--- src/main/java/de/ace/html2pdf/config/exception/GlobalExceptionHandler.java
+++ src/main/java/de/ace/html2pdf/config/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package de.ace.html2pdf.config.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -10,14 +11,14 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.I_AM_A_TEAPOT;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(PdfException.class)
+    @ExceptionHandler(PdfException.class)
     public ResponseEntity<byte[]> pdfExceptionHandler(PdfException exception) {
         return ResponseEntity.status(I_AM_A_TEAPOT).body(exception.getMessage().getBytes(StandardCharsets.UTF_8));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(IOException.class)
+    @ExceptionHandler(IOException.class)
     public ResponseEntity<byte[]> IOExceptionHandler(IOException exception) {
         return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(exception.getMessage().getBytes(StandardCharsets.UTF_8));
     }
